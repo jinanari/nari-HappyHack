15a8b82709307cef03980db683170ae6776da5ab
成山仁 <jinnariyama@seizanjinnoMacBook-Air.local>
2022-09-11 18:40:07 +0900

    変更した内容をメモ

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function($){
+    $('.bg-swicher').bgSwitcher({
+        images: ['images/background1.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg','images/background5.jpg'],
+        interval: 5000,
+        loop: true
+    });
+
+
+    $(window).scroll(function (){
+        var targetElement = $('.fadein').offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if(scroll > targetElement - windowHeight + 200){
+            $('.fadein').css('opacity','1');
+            $('.fadein').css('transform','translateY(0)');
+        }
+    });
+
+    $('.slider').slick({
+        arrows: true,
+        autoplay: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '20%',
+        slidesToShow: 2,
+    });
+
+});
+
